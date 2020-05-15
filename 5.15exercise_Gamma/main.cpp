4d455b95afd7b26fd73cb3fcd415f103fca5ab08
FLH0202 <FLH0202@163.com>
2020-05-15 14:51:00 +0800

    last

--- 5.15exercise_Gamma/main.cpp
+++ 5.15exercise_Gamma/main.cpp
@@ -1,31 +1,33 @@
-#include <iostream>
 #include<opencv2/opencv.hpp>
 
 
+//¶¨ÒåGammaº¯Êý
 void Gamma(cv::Mat& src, cv::Mat& dst, float K)
 {
 	uchar LUT[256];
 	src.copyTo(dst);
 	for (int i = 0; i < 256; i++) 
 	{
-		//float f = (i + 0.5f) / 255;
 		float f = i / 255.0;
 		f = pow(f, K);
-		//LUT[i] = cv::saturate_cast<uchar>(f*255.0f-0.5f);
 		LUT[i] = cv::saturate_cast<uchar>(f*255.0);
 	}
 
-	if (dst.channels() == 1) {
+	if (dst.channels() == 1)
+	{
 		cv::MatIterator_<uchar> it = dst.begin<uchar>();
 		cv::MatIterator_<uchar> it_end = dst.end<uchar>();
-		for (; it != it_end; ++it) {
+		for (; it != it_end; ++it) 
+		{
 			*it = LUT[(*it)];
 		}
 	}
-	else {
+	else
+	{
 		cv::MatIterator_<cv::Vec3b> it = dst.begin<cv::Vec3b>();
 		cv::MatIterator_<cv::Vec3b> it_end = dst.end<cv::Vec3b>();
-		for (; it != it_end; ++it) {
+		for (; it != it_end; ++it) 
+		{
 			(*it)[0] = LUT[(*it)[0]];
 			(*it)[1] = LUT[(*it)[1]];
 			(*it)[2] = LUT[(*it)[2]];
@@ -43,6 +45,6 @@ int main() {
 
 	imshow("src", src);
 	imshow("dst", dst);
-	cv::waitKey(0);
+    waitKey(0);
 }
    
